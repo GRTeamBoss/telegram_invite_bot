bdd80485e2734c4bf87295491d69e44fbfbc2059
GRTeamBoss <rabbitwow97@gmail.com>
2026-04-29 15:19:51 +0500

    add data

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -14,8 +14,12 @@ const dataPhones = new Database().data
 async function checkIfPhoneExists(phone) {
   let result = '';
   for (let i = 0; i < dataPhones.length; i++) {
-    if (dataPhones[i].toString().includes(phone)) {
-      result = `https://invite.com/?phone=${dataPhones[i]}`
+    for (let j = 0; j < dataPhones[i].length; j++) {
+      if (dataPhones[i][j]["тел ракам"] !== null) {
+        if (dataPhones[i][j]["тел ракам"].toString().replaceAll("-", "").includes(phone)) {
+          result = `https://invite.com/?phone=${dataPhones[i][j]["тел ракам"].toString().replaceAll("-", "")}`
+        }
+      }
     }
   }
   return result
@@ -30,15 +34,15 @@ async function sendPhoneLink(ctx, result) {
 }
 
 bot.start(async (ctx) => await ctx.reply(`Hello ${ctx.chat.username || ctx.chat.first_name}`, sendPhone));
-bot.help(async (ctx) => await ctx.reply('Send me number in format like: /invite [+998xxYYYxxYY] or [998xxYYYxxYY] or [xxYYYxxYY]'));
-bot.hears(/invite \+?[\d]{9,12}$/, async (ctx) => {
+bot.help(async (ctx) => await ctx.reply('Send me number in format like: /invite [xxYYYxxYY]'));
+bot.hears(/invite \+?[\d]{9}$/, async (ctx) => {
   const commandSplitted = ctx.message.text.split(" ")
   const phone = commandSplitted[1]
   let result = await checkIfPhoneExists(phone);
   await sendPhoneLink(ctx, result)
 })
 bot.on('contact', async (ctx) => {
-  const phone = ctx.message.contact.phone_number;
+  const phone = ctx.message.contact.phone_number.slice(3);
   let result = await checkIfPhoneExists(phone);
   await sendPhoneLink(ctx, result)
 })
