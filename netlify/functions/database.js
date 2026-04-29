966313d824b865d0a70fb26b8c2f43b411ccb468
GRTeamBoss <rabbitwow97@gmail.com>
2026-04-29 15:38:34 +0500

    add path

--- netlify/functions/database.js
+++ netlify/functions/database.js
@@ -1,14 +1,17 @@
 import {readFile} from "fs"
+import path from "path"
 
 export class Database {
   data = []
   constructor() {
-    readFile("./netlify/functions/json/output1.json", "utf-8", (err, data) => {
+    const folderPath1 = path.join(__dirname, "json", "output1.json")
+    const folderPath2 = path.join(__dirname, "json", "output2.json")
+    readFile(folderPath1, "utf-8", (err, data) => {
       if (err) throw err;
       const json = JSON.parse(data);
       this.data.push(json)
     })
-    readFile("./netlify/functions/json/output2.json", "utf-8", (err, data) => {
+    readFile(folderPath2, "utf-8", (err, data) => {
       if (err) throw err;
       const json = JSON.parse(data);
       this.data.push(json)
